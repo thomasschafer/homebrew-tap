89aac12f8fb2e23249eda2cc53fdc44f626390bb
Tom Schafer <54135831+thomasschafer@users.noreply.github.com>
2025-06-15 16:39:06 +0100

    frep 0.1.2

--- Formula/frep.rb
+++ Formula/frep.rb
@@ -1,8 +1,8 @@
 class Frep < Formula
   desc "Fast find-and-replace CLI"
   homepage "https://github.com/thomasschafer/frep"
-  url "https://github.com/thomasschafer/frep/archive/refs/tags/v0.1.1.tar.gz"
-  sha256 "3eb837d91f4005e78ab7dc55bdc3829d698dd851c636fbfcc35798de9841ea91"
+  url "https://github.com/thomasschafer/frep/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "39ba43e44884aa2f8d3394cfca39a28c66c264b741894b3092e7d1350a502f6a"
   license "MIT"
 
   depends_on "rust" => :build
